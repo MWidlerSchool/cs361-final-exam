90ee1929fba989ee70683214de510ec6c9738c3c
Michael Widler <MichaelWidler@Yahoo.com>
2020-03-18 11:25:29 -0700

    Change individual calls in main.rb to iterated loop

--- main.rb
+++ main.rb
@@ -15,14 +15,20 @@ bikes = [
 ]
 
 total_price = 0
-total_price += bikes[0].weekly_rate
-total_price += bikes[0].tail_pack.cost
-total_price += bikes[1].daily_rate * 7
-total_price += bikes[1].panniers[0].price
-total_price += bikes[1].panniers[1].price
-total_price += bikes[2].weekly_rate
-total_price += bikes[2].luggage.price
-total_price += bikes[3].weekly_rate
-total_price += bikes[3].luggage.price
+
+bikes.each do |cur_bike|
+  total_price += cur_bike.weekly_rate
+  total_price += cur_bike.equipment_cost
+end
+
+#total_price += bikes[0].weekly_rate
+#total_price += bikes[0].tail_pack.cost
+#total_price += bikes[1].daily_rate * 7
+#total_price += bikes[1].panniers[0].price
+#total_price += bikes[1].panniers[1].price
+#total_price += bikes[2].weekly_rate
+#total_price += bikes[2].luggage.price
+#total_price += bikes[3].weekly_rate
+#total_price += bikes[3].luggage.price
 
 puts "Total price: #{total_price}"
